ec8d19e02cfdf4615aec2c9cffe5235c5bc710d7
jeongsangtae <wjdtkdxo55@naver.com>
2024-12-18 03:39:37 +0900

    Float Chat - Navigate add & router path edit

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DirectChatPage from "./pages/DirectChatPage";
 import GroupChatPage from "./pages/GroupChatPage";
@@ -6,14 +6,18 @@ import RootLayout from "./pages/RootLayout";
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element: <Navigate to="/me" replace />, // 루트 경로 접근 시 /me로 리다이렉트
+  },
+  {
     path: "/me",
     element: <RootLayout />,
     children: [
-      { index: true, element: <MainPage /> },
-      { path: ":userId", element: <DirectChatPage /> },
+      { index: true, element: <MainPage /> }, //기본 경로에서 MainPage 표시
+      { path: ":userId", element: <DirectChatPage /> }, // 1:1 채팅 페이지
     ],
   },
-  { path: "/:roomId", element: <GroupChatPage /> },
+  { path: "/:roomId", element: <GroupChatPage /> }, // 그룹 채팅 페이지
 ]);
 
 export default router;
