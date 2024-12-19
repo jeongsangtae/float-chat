448465a60272d40bbb25f72b88b837a8a285b39b
jeongsangtae <wjdtkdxo55@naver.com>
2024-12-20 02:55:46 +0900

    Float Chat - router structure, MainHeader path edit

--- frontend/src/components/Layout/MainHeader.tsx
+++ frontend/src/components/Layout/MainHeader.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const MainHeader = () => {
   return (
     <>
-      <Link to={"/me"}>메인 페이지</Link>/
-      <Link to={"userId"}>다이렉트 채팅방</Link>/
-      <Link to={"/roomId"}>그룹 채팅방</Link>
+      <Link to="/me">메인 페이지</Link>/
+      <Link to="/me/userId">다이렉트 채팅방</Link>/
+      <Link to="/roomId">그룹 채팅방</Link>
     </>
   );
 };
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -6,20 +6,22 @@ import RootLayout from "./pages/RootLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Navigate to="/me" replace />, // 루트 경로 접근 시 /me로 리다이렉트
-  },
-  {
-    path: "/me",
+    path: "/", // RootLayout이 모든 경로에 적용
     element: <RootLayout />,
     children: [
-      { index: true, element: <MainPage /> }, //기본 경로에서 MainPage 표시
-      // { path: ":userId", element: <DirectChatPage /> }, // 1:1 채팅 페이지
-      { path: "userId", element: <DirectChatPage /> },
+      { index: true, element: <Navigate to="/me" replace /> }, // 루트 경로 접근 시 /me로 리다이렉트
+      {
+        path: "me",
+        children: [
+          { index: true, element: <MainPage /> }, //기본 /me 경로
+          // { path: ":userId", element: <DirectChatPage /> }, // 1:1 채팅 페이지
+          { path: "userId", element: <DirectChatPage /> },
+        ],
+      },
+      // { path: "/:roomId", element: <GroupChatPage /> }, // 그룹 채팅 페이지
+      { path: "roomId", element: <GroupChatPage /> },
     ],
   },
-  // { path: "/:roomId", element: <GroupChatPage /> }, // 그룹 채팅 페이지
-  { path: "roomId", element: <GroupChatPage /> },
 ]);
 
 export default router;
