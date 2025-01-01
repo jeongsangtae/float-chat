1e6c1e98247750572c7480692c9dc82b2a4da51c
jeongsangtae <wjdtkdxo55@naver.com>
2025-01-02 05:16:17 +0900

    Float Chat - side bar edit & signup content add

--- frontend/src/components/Layout/SideBar.tsx
+++ frontend/src/components/Layout/SideBar.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
+
+import { ModalProps } from "../../types";
 import GroupChats from "../GroupChats/GroupChats";
+import Login from "../Users/Login";
+import Signup from "../Users/Signup";
+import CreateGroupChat from "../GroupChats/CreateGroupChat";
 
 const SideBar = () => {
+  type ModalType = "login" | "signup" | "createGroupChat" | null;
+
+  const [activeModal, setActiveModal] = useState<ModalType>(null);
+
+  const toggleModalHandler = (modalType: ModalType) => {
+    setActiveModal((prev) => (prev === modalType ? null : modalType));
+  };
+
+  const modals: {
+    type: ModalType;
+    label: string;
+    component: React.ComponentType<ModalProps>;
+  }[] = [
+    { type: "login", label: "로그인", component: Login },
+    { type: "signup", label: "회원가입", component: Signup },
+    {
+      type: "createGroupChat",
+      label: "+",
+      component: CreateGroupChat,
+    },
+  ];
+
   return (
     <>
       <GroupChats />
+      {modals.map(({ type, label, component: Component }) => (
+        <div key={type}>
+          <button onClick={() => toggleModalHandler(type)}>{label}</button>
+          {activeModal === type && (
+            <Component onToggle={() => toggleModalHandler(type)} />
+          )}
+        </div>
+      ))}
     </>
   );
 };
--- frontend/src/components/Users/Signup.tsx
+++ frontend/src/components/Users/Signup.tsx
@@ -1,8 +1,133 @@
+import React, { useState } from "react";
+
 import AuthModal from "../UI/AuthModal";
 import { ModalProps } from "../../types";
 
+interface signupDataType {
+  email: string;
+  nickname: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
+
 const Signup = ({ onToggle }: ModalProps) => {
-  return <AuthModal onToggle={onToggle}>회원가입 모달</AuthModal>;
+  // 환경 변수에서 API URL 가져오기
+  const apiURL = import.meta.env.VITE_API_URL;
+
+  const [signupData, setSignupData] = useState<signupDataType>({
+    email: "",
+    nickname: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  // const [error, setError] = useState<boolean>(false);
+  // const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSignupData({ ...signupData, [name]: value });
+  };
+
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // try {
+    //   const response = await fetch(`${apiURL}/signup`, {
+    //     method: "POST",
+    //     body: JSON.stringify(signupData),
+    //     headers: { "Content-Type": "application/json" },
+    //     credentials: "include",
+    //   });
+
+    //   if (!response.ok) {
+    //     const errorData = await response.json();
+    //     setError(true);
+    //     setErrorMessage(errorData.message);
+    //     return null;
+    //   }
+
+    //   console.log("회원가입 성공");
+    //   onToggle();
+    //   // setError(false)
+    // } catch (error) {
+    //   console.error("에러 내용:", error);
+    //   alert(
+    //     "회원가입 중에 문제가 발생했습니다. 새로고침 후 다시 시도해 주세요."
+    //   );
+    // }
+    console.log(signupData, "회원가입 성공");
+  };
+
+  return (
+    <AuthModal onToggle={onToggle}>
+      <form onSubmit={submitHandler}>
+        <h2>회원가입</h2>
+        <label htmlFor="email">이메일</label>
+        <div>
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            value={signupData.email}
+            onChange={inputChangeHandler}
+          />
+        </div>
+
+        <label htmlFor="nickname">닉네임</label>
+        <div>
+          <input
+            required
+            type="text"
+            id="nickname"
+            name="nickname"
+            value={signupData.nickname}
+            onChange={inputChangeHandler}
+          />
+        </div>
+
+        <label htmlFor="username">사용자명</label>
+        <div>
+          <input
+            required
+            type="text"
+            id="username"
+            name="username"
+            value={signupData.username}
+            onChange={inputChangeHandler}
+          />
+        </div>
+
+        <label htmlFor="password">비밀번호</label>
+        <div>
+          <input
+            required
+            type="password"
+            id="password"
+            name="password"
+            value={signupData.password}
+            onChange={inputChangeHandler}
+          />
+        </div>
+
+        <label htmlFor="confirm-password">비밀번호 확인</label>
+        <div>
+          <input
+            required
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            value={signupData.confirmPassword}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        {/* {error && <p>{errorMessage}</p>} */}
+        <button type="submit">가입</button>
+      </form>
+    </AuthModal>
+  );
 };
 
 export default Signup;
