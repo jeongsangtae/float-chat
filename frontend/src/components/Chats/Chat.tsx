2f7379ddd6092708303b52fd89a1bae29cea5a24
jeongsangtae <wjdtkdxo55@naver.com>
2025-04-19 04:03:41 +0900

    Float Chat - chat input resize, css add & chat input textarea scroll add & chat message line break output

--- frontend/src/components/Chats/Chat.tsx
+++ frontend/src/components/Chats/Chat.tsx
@@ -1,10 +1,12 @@
 import { ChatMessage } from "../../types";
 
+import classes from "./Chat.module.css";
+
 const Chat = ({ message }: Pick<ChatMessage, "message">) => {
   return (
-    <>
-      <p>{message}</p>
-    </>
+    <div className={classes["chat-container"]}>
+      <p className={classes["chat-message"]}>{message}</p>
+    </div>
   );
 };
 
--- frontend/src/components/Chats/ChatInput.tsx
+++ frontend/src/components/Chats/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import useAuthStore from "../../store/authStore";
 import useChatStore from "../../store/chatStore";
@@ -6,15 +6,35 @@ import useDirectChatStore from "../../store/directChatStore";
 
 import { RoomId } from "../../types";
 
+import classes from "./ChatInput.module.css";
+
 const ChatInput = ({ roomId }: RoomId) => {
   const { sendMessage } = useChatStore();
   const { userInfo } = useAuthStore();
   const { directChats, getDirectChat } = useDirectChatStore();
+
   const [message, setMessage] = useState<string>("");
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const inputChangeHandler = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ): void => {
+    const textarea = textareaRef.current;
+
+    if (textarea) {
+      // 높이를 초기화하여 scrollHeight 값을 올바르게 계산
+      textarea.style.height = "auto";
+      const newHeight = textarea.scrollHeight; // 새로운 높이 계산
+      textarea.style.height = `${newHeight}px`;
+
+      // 텍스트 입력창의 최대 높이를 설정하고, 그 이상은 스크롤 사용
+      if (newHeight <= 112) {
+        textarea.style.height = `${newHeight}px`;
+      } else {
+        textarea.style.height = "112px";
+      }
+    }
+
     setMessage(event.target.value);
   };
 
@@ -39,15 +59,16 @@ const ChatInput = ({ roomId }: RoomId) => {
   };
 
   return (
-    <>
+    <div className={classes["chat-input-container"]}>
       <textarea
         onChange={inputChangeHandler}
         rows={1}
         value={message}
         placeholder="메시지를 입력해주세요."
+        ref={textareaRef}
       />
       <button onClick={sendMessageHandler}>전송</button>
-    </>
+    </div>
   );
 };
 
