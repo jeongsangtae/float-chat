7748917201e627b1f4059a337771103d89a67c96
jeongsangtae <wjdtkdxo55@naver.com>
2025-04-25 19:29:04 +0900

    Float Chat - group chat context menu close useEffect coment add

--- frontend/src/components/GroupChats/GroupChat.tsx
+++ frontend/src/components/GroupChats/GroupChat.tsx
@@ -73,21 +73,29 @@ const GroupChat = ({
   };
 
   useEffect(() => {
+    // 외부 클릭 감지 함수
     const contectMenuOutsideClickHandler = (event: MouseEvent): void => {
+      // 메뉴창이 열려 있는지 확인
+      // DOM 요소가 존재하는지 확인
+      // 클릭한 요소가 메뉴 내부가 아니라면 (외부 클릭) 메뉴를 닫기
       if (
         contextMenu.visible &&
         contextMenuRef.current &&
+        // contains()는 Node 타입의 인자를 필요로 함
+        // event.target은 EventTarget 타입이므로, 확실하게 Node라고 단언 (as Node)
         !contextMenuRef.current.contains(event.target as Node)
       ) {
         contextMenuCloseHandler();
       }
     };
 
+    // 클릭 이벤트를 전역에 등록
     document.addEventListener("click", contectMenuOutsideClickHandler);
+    // 컴포넌트가 언마운트되거나 context.visible이 변경되면 클린업
     return () => {
       document.removeEventListener("click", contectMenuOutsideClickHandler);
     };
-  }, [contextMenu.visible]);
+  }, [contextMenu.visible]); // visible 상태가 바뀔 때마다 리렌더링
 
   return (
     <>
