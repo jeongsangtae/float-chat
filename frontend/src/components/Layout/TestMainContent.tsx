301428335a58a82870af13a2abf2d1d72997747d
jeongsangtae <wjdtkdxo55@naver.com>
2025-05-08 06:38:18 +0900

    Float Chat - router separation test (login status & non login status)

---
+++ frontend/src/components/Layout/TestMainContent.tsx
@@ -0,0 +1,12 @@
+import Login from "../Users/Login";
+
+const TestMainContent = () => {
+  return (
+    <>
+      <div>비로그인 상태의 메인 콘텐츠 내용</div>
+      <Login />
+    </>
+  );
+};
+
+export default TestMainContent;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+
+import useAuthStore from "./store/authStore";
+
 import router from "./router";
+import authRouter from "./authRouter";
+
 import "./index.css";
+
+const Root = () => {
+  const { isLoggedIn } = useAuthStore();
+
+  return <RouterProvider router={isLoggedIn ? router : authRouter} />;
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <Root />
   </React.StrictMode>
 );
