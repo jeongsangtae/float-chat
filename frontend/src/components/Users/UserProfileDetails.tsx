cde5249289c80b8fe82556efd28de74d899df43d
jeongsangtae <wjdtkdxo55@naver.com>
2025-12-18 06:17:56 +0900

    Float Chat - user profile details mutual friends, mutual group chats output test

--- frontend/src/components/Users/UserProfileDetails.tsx
+++ frontend/src/components/Users/UserProfileDetails.tsx
@@ -1,7 +1,9 @@
-// import useModalStore from "../../store/modalStore";
 import { useState } from "react";
+
 import useModalStore from "../../store/modalStore";
+
 import { ModalProps } from "../../types";
+import Friend from "../Friends/Friend";
 import Modal from "../UI/Modal";
 
 const UserProfileDetails = ({ onToggle }: ModalProps) => {
@@ -11,20 +13,48 @@ const UserProfileDetails = ({ onToggle }: ModalProps) => {
     modalData.initialView ?? "friends"
   );
 
-  console.log(modalData);
+  console.log(modalData.mutualFriends);
+  console.log(modalData.mutualGroupChats);
 
   return (
     <Modal onToggle={onToggle}>
-      {/* <div>
-        {modalData.mutualFriends.map((mutualFriend) => {
-          mutualFriend;
-        })}
-      </div>
       <div>
-        {modalData.mutualGroupChats.map((mutualGroupChat) => {
-          mutualGroupChat;
-        })}
-      </div> */}
+        <button
+          onClick={() => setActiveView("friends")}
+          disabled={activeView === "friends"}
+        >
+          같이 아는 친구
+        </button>
+        <button
+          onClick={() => setActiveView("groups")}
+          disabled={activeView === "groups"}
+        >
+          같이 있는 그룹 채팅방
+        </button>
+      </div>
+      {activeView === "friends" && (
+        <div>
+          <div>테스트 내용</div>
+          {modalData.mutualFriends?.map((mutualFriend) => (
+            <div key={mutualFriend._id}>
+              <div>내용</div>
+              {/* <div>{mutualFriend.nickname}</div>
+              <div>{mutualFriend.avatarImageUrl}</div> */}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {activeView === "groups" && (
+        <div>
+          <div>테스트 내용2</div>
+          {modalData.mutualGroupChats?.map((mutualGroupChat) => (
+            <div key={mutualGroupChat._id}>
+              <div>{mutualGroupChat.title}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </Modal>
   );
 };
