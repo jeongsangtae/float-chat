aff2063c1f120547ed4a03e6e52985a8952429c4
jeongsangtae <wjdtkdxo55@naver.com>
2026-02-21 03:40:07 +0900

    Float Chat - full content opacity test

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useState, useEffect } from "react";
 
 import Notification from "../UI/Notification";
 import SideBar from "./SideBar";
@@ -14,6 +14,8 @@ interface LayoutProps {
 
 const Layout = ({ children, onLeaveChatRoom }: LayoutProps) => {
   const { currentView, groupChatTitle } = useLayoutStore();
+
+  const [fullOpacity, setFullOpacity] = useState(1);
 
   useEffect(() => {
     // 우클릭 감지 함수
@@ -31,13 +33,28 @@ const Layout = ({ children, onLeaveChatRoom }: LayoutProps) => {
   }, []);
 
   return (
-    <div className={classes.wrapper}>
+    <div className={classes.wrapper} style={{ opacity: fullOpacity }}>
       <div className={classes.header}>
         {currentView === "friends" && "친구"}
         {currentView === "directChat" && "다이렉트 메시지"}
         {currentView === "groupChat" && groupChatTitle}
       </div>
       <div className={classes.layout}>
+        <input
+          type="range"
+          min="0.1"
+          max="1"
+          step="0.01"
+          value={fullOpacity}
+          onChange={(e) => setFullOpacity(Number(e.target.value))}
+          className={classes["full-opacity-slider"]}
+          // style={{
+          //   position: "fixed",
+          //   top: 5,
+          //   right: 10,
+          //   zIndex: 9999,
+          // }}
+        />
         <SideBar onLeaveChatRoom={onLeaveChatRoom} />
         <div className={classes["main-content"]}>{children}</div>
         <Notification />
