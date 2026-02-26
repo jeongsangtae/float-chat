18dd26ec9e9d9cae75af8f7c011164de0e48720c
jeongsangtae <wjdtkdxo55@naver.com>
2026-02-27 04:39:26 +0900

    Float Chat - theme mode preset add, edit

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -43,8 +43,42 @@ const Layout = ({ children, onLeaveChatRoom }: LayoutProps) => {
         {currentView === "directChat" && "다이렉트 메시지"}
         {currentView === "groupChat" && groupChatTitle}
       </div>
-      <div className={classes.dark} onClick={() => setTheme("dark")}></div>
-      <div className={classes.blue} onClick={() => setTheme("blue")}></div>
+      <button
+        className={`${classes.dark} ${theme === "dark" ? classes.active : ""}`}
+        onClick={() => setTheme("dark")}
+      ></button>
+      <button
+        className={`${classes.blue} ${theme === "blue" ? classes.active : ""}`}
+        onClick={() => setTheme("blue")}
+      ></button>
+      <button
+        className={`${classes.purple} ${
+          theme === "purple" ? classes.active : ""
+        }`}
+        onClick={() => setTheme("purple")}
+      ></button>
+      <button
+        className={`${classes.emerald} ${
+          theme === "emerald" ? classes.active : ""
+        }`}
+        onClick={() => setTheme("emerald")}
+      ></button>
+      <button
+        className={`${classes.rose} ${theme === "rose" ? classes.active : ""}`}
+        onClick={() => setTheme("rose")}
+      ></button>
+      <button
+        className={`${classes.sunset} ${
+          theme === "sunset" ? classes.active : ""
+        }`}
+        onClick={() => setTheme("sunset")}
+      ></button>
+      <button
+        className={`${classes.orange} ${
+          theme === "orange" ? classes.active : ""
+        }`}
+        onClick={() => setTheme("orange")}
+      ></button>
       <div className={classes.layout}>
         <SideBar
           onLeaveChatRoom={onLeaveChatRoom}
