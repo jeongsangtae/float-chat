27076043600d8bc9953bb97284dad140b9140417
jeongsangtae <wjdtkdxo55@naver.com>
2026-04-14 05:08:54 +0900

    Float Chat - line height content CSS public var add

--- frontend/src/store/socketStore.ts
+++ frontend/src/store/socketStore.ts
@@ -99,7 +99,7 @@ const useSocketStore = create<SocketStore>((set, get) => ({
               (notif) => notif.id !== newMessage.id
             ),
           }));
-        }, 70000);
+        }, 7000);
       });
 
       newSocket.on("groupChatInviteNotification", (groupChatInvite) => {
