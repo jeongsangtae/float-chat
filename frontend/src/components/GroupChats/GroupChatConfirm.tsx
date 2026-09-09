67e7d3db130be9ae7cb701b50388e49dfcc1ade5
jeongsangtae <wjdtkdxo55@naver.com>
2026-09-09 23:56:47 +0900

    Float Chat - host group chat leave confirm CSS test

--- frontend/src/components/GroupChats/GroupChatConfirm.tsx
+++ frontend/src/components/GroupChats/GroupChatConfirm.tsx
@@ -61,8 +61,6 @@ const GroupChatConfirm = ({ onToggle }: ModalProps) => {
     navigate("/me");
   };
 
-  console.log("커밋 테스트");
-
   // 그룹 채팅방 삭제 또는 나가기 버튼 클릭 시 사용자 목록 호출
   useEffect(() => {
     if (
